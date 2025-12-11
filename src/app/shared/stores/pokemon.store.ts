2edcd0c8c8ad6e5777a68acb9dfb145fa2585547
m8xp0w3r <nilsleistner@me.com>
2025-12-11 22:03:14 +0100

    commit

---
+++ src/app/shared/stores/pokemon.store.ts
@@ -0,0 +1,11 @@
+import { signalStore, withState } from "@ngrx/signals";
+import { PokemonInfoApiResult } from "../interfaces/pokemon-info-api-result.interface";
+import { PokemonInfo } from "../interfaces/pokemon-info.interface";
+
+export const PokemonStore = signalStore(
+  { providedIn: "root" },
+  withState({
+    pokemonInfoApiResult: undefined as PokemonInfoApiResult | undefined,
+    pokemonInfo: [] as PokemonInfo[]
+  })
+)
